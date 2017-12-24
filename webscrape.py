573ec49657bc24debaeff82f3d180e033aa9c282
jake94b <jbutler1@csustan.edu>
2017-12-24 10:18:39 -0800

    Added functionality to check for city name

--- webscrape.py
+++ webscrape.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
+import re
 
 BASE_URL = "https://www.jobhired.io/cat/engineering"
 
@@ -7,3 +8,18 @@ cityName = "San Francisco"
 
 soup = bs4.BeautifulSoup(BASE_URL, 'html.parser')
 results = soup.body.find_all(string=re.compile('.*{0}.*'.format(searched_word)), recursive=True)
+
+print 'Found the word "{0}" {1} times\n'.format(cityName, len(results))
+
+for content in results:
+	words = content.split()
+		for index, word in enumerate(words)
+		# If the content contains the search word twice or more this will fire 			for each occurence
+                	if word == cityName:
+                		
+                		# Check if it's a first word
+                	if index != 0:
+
+                		# Check if it's a last word
+            		
+            print '\tWord before: "{0}", word after: "{1}"'.format(before, after)
