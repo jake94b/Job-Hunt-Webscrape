22b36525c7c408e955c98316b9d9a888f2721ac9
jake94b <jbutler1@csustan.edu>
2017-12-23 20:44:16 -0800

    Repurpose search to look in container

--- webscrape.py
+++ webscrape.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 
-BASE_URL = "jobhired.io"
+BASE_URL = "https://www.jobhired.io/cat/engineering"
 
-def get_descriptions(section_url):
-
+def get_cityname(section_url):
+	html = urlopen(section_url).read()
+	soup = BeatifulSoup(html, "lxml")
+	cityname = soup.find("div", "container")
+	
