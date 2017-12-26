db89856581ac016eaee990d6f346b9666bce32de
jake94b <jbutler1@csustan.edu>
2017-12-26 12:56:52 -0800

    Functionality to extract company name from div

--- webscrape.py
+++ webscrape.py
@@ -12,8 +12,24 @@ soup = BeautifulSoup(page.text, “html.parser”)
 
 print(soup.prettify())
 
-def extract_job_title_from_result(soup): 
-  jobs = []
-  return(jobs)
+def extract_job_list_from_result(soup):
+  jobs = [] 
+  for section in soup.find_all(name=”section”, attrs={“class”:”jobs-list”}):
+      for span in div.find_all(name=”span”, attrs={“class”:”title-res”}):
+      jobs.append(span[“title-res”])
+  return jobs
 
-extract_job_title_from_result(soup)
+def extract_company_from_result(soup): 
+  companies = []
+  for div in soup.find_all(name=”div”, attrs={“class”:”col-md-12”}):
+    company = div.find_all(name=”span”, attrs={“class”:”company-res”})
+    if len(company) > 0:
+      for b in company:
+        companies.append(b.text.strip())
+    else:
+      sec_try = div.find_all(name=”span”, attrs={“class”:”result-link-source”})
+        for span in sec_try:
+          companies.append(span.text.strip())
+ return(companies)
+
+
